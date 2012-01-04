46e4febde1c8c1c99f11ee4e0df88707bb8b1382
Shawn Crigger <support@s-vizion.com>
2012-01-04 15:21:43 -0500

    Removed my settings

--- vms/app/config/database.php
+++ vms/app/config/database.php
@@ -50,7 +50,7 @@ $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
 $db['default']['username'] = 'root';
-$db['default']['password'] = 'abstdf';
+$db['default']['password'] = '';
 $db['default']['database'] = 'new-ci';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = 'cms_';
